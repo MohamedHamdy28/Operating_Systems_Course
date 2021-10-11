accc6440bdaedc3ce065a008202f6fdde44c4e85
Mohamed <m.abdelhamid@innopolis.university>
2021-10-11 17:16:50 +0300

    with comments

--- week8/ex2.c
+++ week8/ex2.c
@@ -4,7 +4,7 @@
 #include <string.h>
 int main(){
     char* ptr;
-    int size = 10000*1024*1024;
+    int size = 1000*1024*1024; // when I allocate just 10MB it takes longer for the si and so values to change, so I increased the amount of allocated memory to make it faster
     for(int i=0;i<10;i++)
     {
         ptr= malloc(size);
@@ -13,4 +13,8 @@ int main(){
         printf("Run %d\n",i);
         //free(ptr);
     }
+    /*
+     * my findings: at the very beginning the values of si and so are 60 and 182 respectively then the value of si keeps fluctuating, sometimes it reaches 0
+     * however the values of so stays 0
+    */
 }
