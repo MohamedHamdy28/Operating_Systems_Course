a65d0da0e5fa4c947c2a4558fed30f0447a06128
MohamedHamdy28 <m.abdelhamid@innopolis.university>
2021-10-18 16:31:55 +0300

    m

--- week9/ex1.c
+++ week9/ex1.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 struct page{
     int name;
-    uint8_t counter;
-    int cycle;
+    unsigned int counter;
 };
-int main(){
-    int n,m,hit=0,miss=0;
-    scanf("%d%d",&n,&m);
+int main(int argc, char** argv){
+    int n,hit=0,miss=0;
+    if(argc > 2)
+    {
+        printf("Wrong number of arguments\n");
+        return 1;
+    }
+    sscanf(argv[1], "%d", &n);
+    FILE* fp = fopen("ex1.txt", "r");
+    if(fp == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
     struct page page_table[n];
     for(int i=0;i<n;i++)
     {
         page_table[i].name=0;
     }
-    uint8_t refranced[n];
-    int t,a;
-    int index;
-    for(int i=0;i<m;i++)
+    int refranced[n];
+    for(int i=0;i<n;i++)
     {
-        scanf("%d%d",&t,&a);
-        refranced[a]=1;
+        refranced[i]=0;
+    }
+    int a;
+    int index;
+    while (fscanf(fp,"%d",&a)!=EOF)
+    {
+        refranced[a-1]=1;
         int f=0;
         for (int i=0;i<n;i++)
         {
@@ -29,20 +41,18 @@ int main(){
             {
                 f=1;
                 hit++;
-                page_table[i].cycle=t;
                 break;
             }
         }
         if(f==0)//miss
         {
             miss++;
-            uint8_t mn=225;
+            uint8_t mn = 255;
             for(int i=0;i<n;i++)
             {
                 if(page_table[i].name==0)
                 {
                     index=i;
-
                     break;
                 }
                 if(mn>page_table[i].counter)
@@ -50,27 +60,25 @@ int main(){
                     mn=page_table[i].counter;
                     index=i;
                 }
-                if(mn==page_table[i].counter&&page_table[i].name<page_table[index].name)
+                else if(mn==page_table[i].counter&&page_table[i].name<page_table[index].name)
                 {
                     mn=page_table[i].counter;
                     index=i;
                 }
             }
 
-            refranced[index]=0;
+            refranced[a-1]=0;
             page_table[index].name=a;
             page_table[index].counter=0;
-            page_table[index].cycle=t;
         }
         for(int i=0; i<n; i++)
         {
-            if(i==index)
-                continue;
             page_table[i].counter = page_table[i].counter >> 1;
-            page_table[i].counter = page_table[i].counter | (refranced[i] << 8);
+            page_table[i].counter = page_table[i].counter | (refranced[i] << 7);
         }
         printf("%d\n",f);
     }
 
-    printf("%f\n", hit/(double)miss);
+    printf("Hits: %d, Misses: %d\n", hit, miss);
+    printf("Hits/Misses = %f\n", hit/(double)miss);
 }
