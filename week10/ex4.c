f0b5a0f7e0e2a537154095b3775814c303293fa4
Mohamed <m.abdelhamid@innopolis.university>
2021-10-23 23:59:16 +0300

    m

---
+++ week10/ex4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+
+
+int main()
+{
+    	DIR *dirct = opendir("./tmp");
+	if (dirct == NULL) { return (ERROR); }
+	int c = 0;
+	struct dirent *e;
+	while((e = readdir(dirct)) != NULL)
+	{
+		if(e->d_name[0] == '.') continue;
+
+		struct stat s = {};
+        	char d[100] = "tmp/";
+		stat(strcat(d, e->d_name), &s);
+	
+    	if(s.st_nlink >= 2){
+            c++;
+	}
+    }
+
+    printf("Found %d i-node(s) with hardlink count of two or more\n", cnt);
+}
---
+++ week10/ex4.sh
@@ -0,0 +1,5 @@
+nano file1.txt
+nano file2.txt
+ln file1.txt link1
+gcc ex4.c -o ex4.out
+./ex4.out > ex4.txt
